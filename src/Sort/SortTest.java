96f5cc28a4757d12c39a51449e7a8c68eb449baa
kennycai <kennycai2836@163.com>
2019-03-03 12:35:58 +0800

    归并排序

--- src/Sort/SortTest.java
+++ src/Sort/SortTest.java
@@ -2,6 +2,7 @@ package Sort;
 
 import Sort.impl.BubbleSort;
 import Sort.impl.InsertionSort;
+import Sort.impl.MergeSort;
 import Sort.impl.SelectionSort;
 import org.junit.jupiter.api.Test;
 
@@ -10,9 +11,10 @@ import java.util.Arrays;
 public class SortTest {
     @Test
     public void test () {
-        int [] list = {4,5,6,1,3,2};
-        Sort sort = new SelectionSort();
-        assert(Arrays.equals(sort.sort(list), new int[] {1,2,3,4,5,6}));
-        System.out.println(Arrays.toString(sort.sort(list)));
+        int [] list = {4, 5, 6, 1, 3, 2};
+        Sort sort = new MergeSort();
+        list = sort.sort(list);
+        assert(Arrays.equals(list, new int[] {1, 2, 3, 4, 5, 6}));
+        System.out.println(Arrays.toString(list));
     }
 }
